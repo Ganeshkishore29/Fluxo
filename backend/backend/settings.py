f329d75a198847f3039509ecd4c1f936bf6cd556
Ganeshkishore29 <ganeshkishores29@gmail.com>
2026-01-26 12:28:49 +0530

    Add cloudinary storage support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,6 +91,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- frontend/src/components/SmallProductCard.js
+++ frontend/src/components/SmallProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate} from "react-router-dom";
 
+
 const SmallProductCard = ({ product }) => {
   const navigate = useNavigate();
 
@@ -14,7 +15,7 @@ const SmallProductCard = ({ product }) => {
       onClick={() => navigate(`/product/${product.id}`)}
     >
       <img
-        src={`http://localhost:8000${Image}`}
+        src={`${process.env.REACT_APP_API_BASE_URL}${Image}`}
         alt={product.name}
         className="w-full h-full object-cover transition-all duration-300"
       />
