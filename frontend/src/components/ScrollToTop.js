ad41fb2dc700693584d10d5b2840f3d28dcb2708
Ganeshkishore29 <ganeshkishores29@gmail.com>
2026-02-12 20:08:28 +0530

    Add global ScrollToTop behavior on route change

--- frontend/src/components/ScrollToTop.js
+++ frontend/src/components/ScrollToTop.js
@@ -8,7 +8,7 @@ const ScrollToTop = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant" // or "smooth" if you want animation
+      behavior: "smooth" 
     });
   }, [pathname]);
 
