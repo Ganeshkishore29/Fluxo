523d87fb0774c39cf16fde7d1bceb95b112e58f4
Ganeshkishore29 <ganeshkishores29@gmail.com>
2026-02-13 14:05:35 +0530

    upgrade the login,checkout,signup and register

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api`;
